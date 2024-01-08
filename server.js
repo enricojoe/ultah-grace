51767bbc64b939c249a62475a9b7ccc34124f89d
enricojoe <enrico.joe06@gmail.com>
2024-01-08 17:07:53 +0700

    baru

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 
-app.set('views', './views');
+app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + "/public"));
